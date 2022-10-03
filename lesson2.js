a68899b537078bd217ad7576827858aa2e68718f
FoxWithMatches <freiheiit92@mail.ru>
2022-10-03 14:05:26 +0300

    les1

---
+++ lesson2.js
@@ -0,0 +1,40 @@
+// console.log(nameMy);
+// console.log(5 > 4);
+// console.log("ананас" > "яблоко");
+// console.log("13" > "12");
+// console.log(undefined == null);
+// console.log(undefined === null);
+// console.log(null == "\n0\n");
+// console.log(null === +"\n0\n");
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log(true + false);
+// console.log(6 / "3");
+// console.log("2" * "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 + 5);
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log(7 / 0);
+// console.log(" -9 " + 5);
+// console.log(" -9 " - 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log(" \t\n" - 2);
+      
+// const town = ["paris", "london", "nn"]
+// town[0] = "milan"
+// town.push("saransk");
+// town.push("orel");
+// console.log(town);
+// // town.pop();
+// town.shift("msk");
+// console.log(town);
+
+
+const films = ["Omen", "13 friends", "COde", "kino"];
+console.log(films[0]);
+films.push("new");
+films.unshift("one");
+console.log(films[films.length - 1]);
+console.log(films)
---
+++ script.js
@@ -0,0 +1,21 @@
+const myName = "Алиса"
+const myAge = 18
+const userData = `имя пользователя: ${myName}, возраст: ${myAge}`;
+
+let user = {
+    name: "Alice",
+    age: 30
+};
+
+const number = 10323123232333333334n;
+
+let a = null;
+
+console.log(typeof user)
+console.log(typeof a)
+
+
+
+let b = 23;
+String(b);
+console.log( typeof String(b))
